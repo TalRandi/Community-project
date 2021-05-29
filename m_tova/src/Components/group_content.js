109d1384df7db865fe9d9d1ed53fcb28991dbbcc
Adi-Lei <adileibovich1997@gmail.com>
2021-05-29 12:59:36 +0300

    group content update, still working on it

--- m_tova/src/Components/group_content.js
+++ m_tova/src/Components/group_content.js
@@ -1,22 +1,33 @@
 import { useState } from 'react';
-import {storage, db} from '../Firebase/firebase';
+import { storage, db } from '../Firebase/firebase';
 
 const GroupContent = props => {
 
     const course_name = props.course_name;
     const [students_shared_content, setStudentSharedContent] = useState([])
-    
+    const [student_details, setStudentDetails]=useState({})
+
+    // const getDetailStudent =(e)=>{
+    //     console.log(e.target.id);
+    //     storage.ref().child(`${course_name}/class${student_details.class_number}/${student_details.student_name}`).listAll().then(list => {
+    //         console.log(`${course_name}/class${student_details.class_number}/${student_details.student_name}`);
+    //         //console.log()
+    //     })
+
+    // }
+
+
     let list
-    const getData = async () =>{
-        
+    const getData = async () => {
+
         await db.collection("studentContent").where("course_name", "==", course_name)
             .get()
             .then(querySnapshot => {
                 list = querySnapshot.docs.map(item => {
                     return (
-                        <div className = "shared-content-card" key = {item.data().student_name}>
+                        <div className="shared-content-card"  key={item.data().student_name} >
                             <ul>{item.data().course_name}</ul>
-                            <ul>{item.data().class_number}</ul>
+                            <ul>שיעור {item.data().class_number}</ul>
                             <ul>{item.data().student_name}</ul>
                         </div>
                     )
@@ -24,15 +35,17 @@ const GroupContent = props => {
                 setStudentSharedContent(list)
             })
     }
+ 
     return (
-        <div>
-            <button onClick = {getData}>click here</button>
-            <div className="internal_content">
-                {students_shared_content}
+       
+    <div>
+        <button onClick={getData}>click here</button>
+        <div className="internal_content">
+            {students_shared_content}
 
-            </div>
         </div>
+    </div>
     );
 }
- 
+
 export default GroupContent;
